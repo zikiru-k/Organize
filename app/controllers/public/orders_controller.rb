bd100b549d1fd25d4467a68193c1822aa6cb8e92
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2024-02-11 15:13:36 +0000

    [Add] 発注情報の保存

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,7 +10,23 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    if @order.save
+    @order.group_id = 1 # FIXME: グループ機能がついたらここのロジック変更する
+
+    if @order.save!
+      # 新規商品があればitemに新規登録される
+      # index:配列の番号が入る
+      params[:new_items][:name].each_with_index do |name, index|
+        next if name.blank?
+        item = Item.find_or_create_by(name: name) do |item|
+          item.code = params[:new_items][:code][index]
+          item.capacity = params[:new_items][:capacity][index]
+          item.site = params[:new_items][:site][index]
+        end
+
+        @order.order_details.create(item_id: item.id, amount:  params[:new_items][:amount][index])
+      end
+
+
       redirect_to orders_path
     else
       render :new
@@ -20,6 +36,17 @@ class Public::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  private
+
+  def order_params
+    # モデル名_attributesが子のモデルに保存する要素
+    #   :id, :_destroyをつけることで、編集と削除が可能になる
+    params.require(:order).permit(
+      order_details_attributes: [:id, :item_id, :amount, :_destroy])
+      # , :order_tag_relation_id
+  end
+end
 
   # def edit
   #   @order = Order.find(params[:id])
@@ -38,14 +65,3 @@ class Public::OrdersController < ApplicationController
   #   Order.destroy(params[:id])
   #   redirect_to orders_path
   # end
-
-  private
-
-  def order_params
-    # モデル名_attributesが子のモデルに保存する要素
-    #   :id, :_destroyをつけることで、編集と削除が可能になる
-    params.require(:order).permit(
-      order_derails_attributes: [:id, :item_id, :amount, :_destroy])
-      #items_attributes: [:id, :order_tag_relation_id, :name, :code, :capacity, :site, :_destroy]
-  end
-end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,6 @@ class Order < ApplicationRecord
   belongs_to :group
   has_many :comments, dependent: :destroy
   has_many :order_details, dependent: :destroy
-  belongs_to :orderer, class_name: "Customer"
 
   # accepts_nested_attributes_forは、親モデルを保存するときに、Associationで関連づけた子モデルも一緒に保存することができるメソッド
   # reject_ifは、入力フォームを追加しているもののすべてが空白の場合にリジェクトする
