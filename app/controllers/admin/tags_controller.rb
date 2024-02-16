7423f63d4d4ec7d259a681fab677ec3198a4b41e
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2024-02-16 14:43:03 +0000

    [Add] 管理者側：会員一覧関係

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,3 +1,18 @@
 class Admin::TagsController < ApplicationController
   before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
